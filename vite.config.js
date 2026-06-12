b62b564d0d5cd76ce161cac693a9864452e3ed33
Jorge Luiz <defiant.jr@outlook.com>
2026-06-12 13:03:33 -0300

    correcao site

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/*
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,6 +7,25 @@ export default defineConfig({
   server: {
     host: '127.0.0.1',
     port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    minify: 'esbuild',
+  },
+});
+*/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: [
+      'experimental-jrnotes.umejya.easypanel.host'
+    ],
   },
   build: {
     outDir: 'dist',
